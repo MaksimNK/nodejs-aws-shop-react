88194ac65a179a5dcf57fe3c4fcdeb2b27d64f1a
MaksimNK <maksmozir@gmail.com>
2025-03-08 18:37:54 +0300

    fix: update path to upload file

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://xhoulf9w6f.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://ulkgswjvsg.execute-api.us-east-1.amazonaws.com/prod/import?name=test.csv",
   bff: "https://xhoulf9w6f.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
